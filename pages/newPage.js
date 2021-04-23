f75b29c4f9030f3bbed5e63b30844b8715669118
TheJayDuck <therealjayduck@gmail.com>
2021-04-23 12:27:04 +0300

    Improves scaling and overall interface

--- pages/newPage.js
+++ pages/newPage.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/newPage.module.css'
 
-const data = [...Array(15).keys()].map(n => ({
+const data = [...Array(55).keys()].map(n => ({
   title: "Pretty Boy Detective",
-  url: "https://i.ibb.co/2dcp1RR/CQO6-Nvu-Uc-AAz-Q1-Y.jpg",
+  coverUrl: "https://i.ibb.co/2dcp1RR/CQO6-Nvu-Uc-AAz-Q1-Y.jpg",
   chapter: 25,
   volume: 1,
+  status: "Dropped", //Reading, Paused, Dropped, Planning
 }))
 
 function NovelCard({entry}) {
@@ -13,21 +14,27 @@ function NovelCard({entry}) {
     <div className={styles.activityEntry}>
       <div className={styles.wrap}>
         <div className={styles.list}>
-          <img className={styles.cover} src={entry.url}/>
-          <div className={styles.details}>
+          <img className={styles.cover} src={entry.coverUrl}/>
             <p className={styles.title}>{entry.title}</p>
+          <div className={styles.details}>
             <div>
               <span className={styles.status}>Current Chapter: {entry.chapter}</span>
               <br/>
               <span className={styles.status}>Current Volume: {entry.volume}</span>
+              <hr/>
+              <span className={styles.status}>Status: {entry.status}</span>
+              <hr/>
+              <a title="Increase Progress" className={`${styles.icon} fas fa-plus ${styles.faPlus}`}></a>
+              <a title="Decrease Progress" className={`${styles.icon} fas fa-minus ${styles.faMinus}`}></a>
+              <a title="Info" className={`${styles.icon} fas fa-info ${styles.faInfo}`}></a>
             </div>
 
           </div>
-          <div className={styles.progress}>
-            <a className={`${styles.icon} fas fa-plus ${styles.faPlus}`}></a>
-            <a className={`${styles.icon} fas fa-minus ${styles.faMinus}`}></a>
-            <a className={`${styles.icon} fas fa-info ${styles.faInfo}`}></a>
-          </div>
+          {/* <div className={styles.progress}>
+            <a title="Increase Progress" className={`${styles.icon} fas fa-plus ${styles.faPlus}`}></a>
+            <a title="Decrease Progress" className={`${styles.icon} fas fa-minus ${styles.faMinus}`}></a>
+            <a title="Info" className={`${styles.icon} fas fa-info ${styles.faInfo}`}></a>
+          </div> */}
         </div>
 
       </div>
@@ -43,6 +50,10 @@ export default function NewPage() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
         <meta name="mobile-web-app-capable" content="yes"/>
+
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="public/favicon.ico"/>
+
       </Head>
       
       <main className={styles.main}>
@@ -60,11 +71,13 @@ export default function NewPage() {
         </div>
 
       </main>
-      <a className={`${styles.newBook} fas fa-plus ${styles.faPlus}`} href="/newBook"/>
+      <a title="Add New Book" className={`${styles.newBook} fas fa-plus ${styles.faPlus}`} href="/"/>
       <footer className={styles.footer}>
         <p>Finished Books: x</p>
         <br/>
         <p>Total Pages Read: x</p>
+        <br/>
+        <a title="Check Out My Github" href="https://github.com/thejayduck" className="fab fa-github" target="_blank"/>
       </footer>
     </div>
   )
