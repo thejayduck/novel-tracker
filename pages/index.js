9cdfc5a507816680dda1139fc34a5c76f1c3ec8d
Nobbele <realnobbele@gmail.com>
2021-04-24 14:40:14 +0200

    adds jikan

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import NewBook from './newBook'
+import jikanjs from 'jikanjs'
 
-const data = [...Array(32).keys()].map(n => ({
+const data = [...Array(5).keys()].map(n => ({
   title: "Pretty Boy Detective",
   coverUrl: "https://i.ibb.co/2dcp1RR/CQO6-Nvu-Uc-AAz-Q1-Y.jpg",
   chapter: 25,
@@ -10,17 +11,29 @@ const data = [...Array(32).keys()].map(n => ({
   status: "Reading", //Reading, Paused, Dropped, Planning
 }))
 
-function NovelCard({entry}) {
+jikanjs.search("manga", "gamer").then(({ results }) => {
+  results.forEach(ln => {
+    data.push({
+      title: ln.title,
+      coverUrl: ln.image_url,
+      chapter: 0,
+      volume: 0,
+      status: "Reading",
+    });
+  });
+});
+
+function NovelCard({ entry }) {
   return (
     <div className={styles.activityEntry}>
       <div className={styles.wrap}>
         <div className={styles.list}>
           <p className={styles.title}>{entry.title}</p>
-          <img className={styles.cover} src={entry.coverUrl}/>
+          <img className={styles.cover} src={entry.coverUrl} />
           <div className={styles.details}>
             <div>
               <span className={styles.status}>Current Chapter: {entry.chapter}</span>
-              <hr/>
+              <hr />
               {/* <span className={styles.status}>Current Volume: {entry.volume}</span>
               <hr/>
               <span className={styles.status}>Status: {entry.status}</span>
@@ -44,33 +57,33 @@ export default function Home() {
         <title>Book Library</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-        <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="mobile-web-app-capable" content="yes" />
 
-        <meta name="msapplication-TileColor" content="#ffffff"/>
-        <meta name="msapplication-TileImage" content="public/favicon.ico"/>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="public/favicon.ico" />
 
       </Head>
-      
+
       <main className={styles.main}>
         <div className={styles.searchContainer}>
           <form action="/" className={styles.searchBar}>
-            <i className={`${styles.icon} fas fa-search ${styles.faSearch}`}/>
-            <input type="text" placeholder="Search..." name="search" maxLength="50"/>
+            <i className={`${styles.icon} fas fa-search ${styles.faSearch}`} />
+            <input type="text" placeholder="Search..." name="search" maxLength="50" />
           </form>
         </div>
         <div className={styles.activityContainer}>
           <div className={styles.activityFeed}>
-            {data.map(entry => (<NovelCard entry={entry}/>))}
+            {data.map(entry => (<NovelCard entry={entry} />))}
           </div>
         </div>
       </main>
-      <a title="Add New Book" className={`${styles.newBook} fas fa-plus ${styles.faPlus}`} href="/"/>
+      <a title="Add New Book" className={`${styles.newBook} fas fa-plus ${styles.faPlus}`} href="/" />
       <footer className={styles.footer}>
         <p>Finished Books: x</p>
-        <br/>
+        <br />
         <p>Total Pages Read: x</p>
-        <br/>
-        <a title="Check Out My Github" href="https://github.com/thejayduck" className="fab fa-github" target="_blank"/>
+        <br />
+        <a title="Check Out My Github" href="https://github.com/thejayduck" className="fab fa-github" target="_blank" />
       </footer>
       {/* <NewBook/> */}
     </div>
