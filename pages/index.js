2b73f9d109d286a9aebf4d3dda4b8645d058dfab
TheJayDuck <therealjayduck@gmail.com>
2021-04-24 15:18:54 +0300

    Improves Interface

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,78 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import NewBook from './newBook'
+
+const data = [...Array(32).keys()].map(n => ({
+  title: "Pretty Boy Detective",
+  coverUrl: "https://i.ibb.co/2dcp1RR/CQO6-Nvu-Uc-AAz-Q1-Y.jpg",
+  chapter: 25,
+  volume: 1,
+  status: "Reading", //Reading, Paused, Dropped, Planning
+}))
+
+function NovelCard({entry}) {
+  return (
+    <div className={styles.activityEntry}>
+      <div className={styles.wrap}>
+        <div className={styles.list}>
+          <p className={styles.title}>{entry.title}</p>
+          <img className={styles.cover} src={entry.coverUrl}/>
+          <div className={styles.details}>
+            <div>
+              <span className={styles.status}>Current Chapter: {entry.chapter}</span>
+              <hr/>
+              {/* <span className={styles.status}>Current Volume: {entry.volume}</span>
+              <hr/>
+              <span className={styles.status}>Status: {entry.status}</span>
+              <hr/> */}
+              <a title="Increase Progress" className={`${styles.icon} fas fa-plus ${styles.faPlus}`}></a>
+              <a title="Decrease Progress" className={`${styles.icon} fas fa-minus ${styles.faMinus}`}></a>
+              <a title="Info" className={`${styles.icon} fas fa-info ${styles.faInfo}`}></a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Book Library</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+        <meta name="mobile-web-app-capable" content="yes"/>
+
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="public/favicon.ico"/>
+
       </Head>
-
+      
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.searchContainer}>
+          <form action="/" className={styles.searchBar}>
+            <i className={`${styles.icon} fas fa-search ${styles.faSearch}`}/>
+            <input type="text" placeholder="Search..." name="search" maxLength="50"/>
+          </form>
+        </div>
+        <div className={styles.activityContainer}>
+          <div className={styles.activityFeed}>
+            {data.map(entry => (<NovelCard entry={entry}/>))}
+          </div>
         </div>
       </main>
-
+      <a title="Add New Book" className={`${styles.newBook} fas fa-plus ${styles.faPlus}`} href="/"/>
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <p>Finished Books: x</p>
+        <br/>
+        <p>Total Pages Read: x</p>
+        <br/>
+        <a title="Check Out My Github" href="https://github.com/thejayduck" className="fab fa-github" target="_blank"/>
       </footer>
+      {/* <NewBook/> */}
     </div>
   )
 }
---
+++ pages/newBook.js
@@ -0,0 +1,15 @@
+import styles from '../styles/NewBook.module.css'
+
+export default function NewBook() {
+    return (
+        <div>
+            <div className={styles.main}>
+                <div className={styles.container}>
+
+                    
+
+                </div>
+            </div>
+        </div>
+    )
+}
