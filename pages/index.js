90594bdfd44fb960f40808fec992b175da92a17b
TheJayDuck <therealjayduck@gmail.com>
2021-04-25 20:02:09 +0300

    Adds Search Functionality and overall improvements
    Co-authored-by: nobbele

--- pages/index.js
+++ pages/index.js
@@ -5,38 +5,47 @@ import NewBook from './newBook'
 import jikanjs from 'jikanjs'
 import { useState, useEffect } from 'react'
 
-const data = [...Array(66).keys()].map(n => ({
-  title: "Pretty Boy Detective",
-  coverUrl: "https://i.ibb.co/2dcp1RR/CQO6-Nvu-Uc-AAz-Q1-Y.jpg",
-  chapter: 25,
-  volume: 1,
-  status: "Reading", //Reading, Paused, Dropped, Planning
-}))
-
-function NovelCard({ entry }) {
+export function CardListWrapper({ data, children }) {
   return (
-    <div className={cardStyle.activityEntry}>
-      <div className={cardStyle.wrap}>
+    <div className={styles.cardListContainer}>
+      <ul className={styles.cardListFeed}>{children}</ul>
+    </div>
+  );
+}
+
+function NovelCard({ entry, onIncrement, onDecrement, onInfoClick }) {
+  return (
+    <div className={cardStyle.wrap}>
         <div className={cardStyle.list}>
-          <p title={entry.title} className={cardStyle.title}>{entry.title}</p>
+          <p title={entry.title} className={cardStyle.title}>
+            {entry.title}
+          </p>
           <img className={cardStyle.cover} src={entry.coverUrl} />
           <div className={cardStyle.details}>
             <div>
-              <span className={cardStyle.status}>Current Chapter: {entry.chapter}</span>
+              <span className={cardStyle.status}>
+                Current Chapter: {entry.chapter}
+              </span>
               <hr />
               {/* <span className={styles.status}>Current Volume: {entry.volume}</span>
               <hr/>
               <span className={styles.status}>Status: {entry.status}</span>
               <hr/> */}
-              <a title="Increase Progress" className="fas fa-plus"></a>
-              <a title="Decrease Progress" className="fas fa-minus"></a>
-              <a title="Info" className="fas fa-info"></a>
+              <a
+                onClick={onIncrement}
+                title="Increase Progress"
+                className="fas fa-plus"
+              ></a>
+              <a
+                onClick={onDecrement}
+                title="Decrease Progress"
+                className="fas fa-minus"
+              ></a>
+              <a onClick={onInfoClick} title="Info" className="fas fa-info"></a>
             </div>
           </div>
         </div>
-
       </div>
-    </div>
   );
 }
 
@@ -54,45 +63,102 @@ function usePersistedState(key, defaultValue) {
 export default function Home() {
   const [darkmode, setDarkmode] = usePersistedState("darkmode", true);
   const [overlay, setOverlay] = useState(false);
+  const [data, setData] = useState([...Array(1).keys()].map(n => ({
+    title: "Pretty Boy Detective",
+    coverUrl: "https://i.ibb.co/2dcp1RR/CQO6-Nvu-Uc-AAz-Q1-Y.jpg",
+    chapter: 25,
+    volume: 1,
+    status: "Reading", //Reading, Paused, Dropped, Planning
+  })));
+  const updateElementInData = (index, updateElementCallback) => {
+    let new_data = [...data];
+    updateElementCallback(new_data[index]);
+    setData(new_data);
+  };
 
   return (
-    <div className={`${styles.container} ${darkmode ? styles.dark : styles.light}`}>
+    <div
+      className={`${styles.container} ${darkmode ? styles.dark : styles.light}`}
+    >
       <Head>
         <title>Book Library</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        />
         <meta name="mobile-web-app-capable" content="yes" />
 
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="public/favicon.ico" />
-
       </Head>
 
       <main className={styles.main}>
         <div className={styles.searchContainer}>
-          <form action="/" className={styles.searchBar}>
+          <div className={styles.searchBar}>
             <i className={`${styles.icon} fas fa-search ${styles.faSearch}`} />
-            <input type="text" placeholder="Search..." name="search" maxLength="50" />
-          </form>
-        </div>
-        <div className={styles.activityContainer}>
-          <div className={styles.activityFeed}>
-            {data.map(entry => (<NovelCard entry={entry} />))}
+            <input
+              type="text"
+              placeholder="Search..."
+              name="search"
+              maxLength="50"
+            />
           </div>
         </div>
+        <CardListWrapper data={data}>
+          {data.map((entry, index) => (
+            <li key={entry.mal_id}>
+              <div className={cardStyle.activityEntry}>
+                  <NovelCard
+                    entry={entry}
+                    onIncrement={() =>
+                      updateElementInData(index, (element) => element.chapter++)
+                    }
+                    onDecrement={() =>
+                      updateElementInData(
+                        index,
+                        (element) =>
+                          (element.chapter = Math.max(0, element.chapter - 1))
+                      )
+                    }
+                    onInfoClick={() => {
+                      console.log("Info");
+                    }}
+                  />
+              </div>
+            </li>
+          ))}
+        </CardListWrapper>
       </main>
-      <footer className={`${styles.footer} ${darkmode ? styles.dark : styles.light}`}>
-        <button onClick={() => setDarkmode(!darkmode)}>Theme</button>
+      <footer
+        className={`${styles.footer} ${darkmode ? styles.dark : styles.light}`}
+      >
+        <button className="fas fa-moon" onClick={() => setDarkmode(!darkmode)}>
+          Theme
+        </button>
         <p>Finished Books: x</p>
         <br />
         <p>Chapters Read: x</p>
         <br />
-        <a title="Check Out My Github" href="https://github.com/thejayduck" className="fab fa-github" target="_blank" />
+        <a
+          title="Check Out TheJayDuck's Github"
+          href="https://github.com/thejayduck"
+          className="fab fa-github"
+          target="_blank"
+        />
+        <a
+          title="Check Out Nobbele's Github"
+          href="https://github.com/nobbele"
+          className="fab fa-github"
+          target="_blank"
+        />
       </footer>
-      {overlay && <NewBook/>}
-      <div className={styles.newBook} onClick={()=> setOverlay(!overlay)}>
-        <a title="Add New Book" className="fas fa-plus"/>
+      {overlay && (
+        <NewBook onAddClicked={(entry) => setData([...data, entry])} />
+      )}
+      <div className={styles.newBook} onClick={() => setOverlay(!overlay)}>
+        <a title="Add New Book" className="fas fa-plus" />
       </div>
     </div>
-  )
+  );
 }
--- pages/newBook.js
+++ pages/newBook.js
@@ -1,60 +1,122 @@
 import styles from '../styles/NewBook.module.css'
 import cardStyle from '../styles/BookCard.module.css'
 import jikanjs from 'jikanjs'
+import { useState, useEffect } from 'react'
+import { CardListWrapper } from './index'
 
-const data = [...Array(0).keys()].map(n => ({
+/*const data = [...Array(0).keys()].map(n => ({
   title: "Pretty Boy Detective",
   coverUrl: "https://i.ibb.co/2dcp1RR/CQO6-Nvu-Uc-AAz-Q1-Y.jpg",
   chapter: 25,
   volume: 1,
   status: "Reading", //Reading, Paused, Dropped, Planning
-}))
+}))*/
 
-jikanjs.search("manga", "Pretty Boy").then(({ results }) => {
-  results.forEach(ln => {
-    data.push({
-      mal_id: ln.mal_id,
-      title: ln.title,
-      coverUrl: ln.image_url,
-      type: ln.type,
-      chapter: 1,
-      volume: 0,
-      status: "Reading",
-    });
-  });
-});
 
-function NovelCard({ entry }) {
+// jikanjs.search("manga", "get").then(({ results }) => {
+//   results.forEach(ln => {
+//     data.push({
+//       mal_id: ln.mal_id,
+//       title: ln.title,
+//       coverUrl: ln.image_url,
+//       type: ln.type,
+//       chapter: 1,
+//       volume: 0,
+//       status: "Reading",
+//     });
+//   });
+// });
+function NovelCard({ entry, onAddClicked }) {
   return (
-    <div className={cardStyle.activityEntry}>
+    <>
       <div className={cardStyle.wrap}>
         <div className={cardStyle.list}>
-          <p title={`${entry.title} | ${entry.type}`} className={cardStyle.title}>{entry.title}</p>
+          <p
+            title={`${entry.title} | ${entry.type}`}
+            className={cardStyle.title}
+          >
+            {entry.title}
+          </p>
           <img className={cardStyle.cover} src={entry.coverUrl} />
         </div>
       </div>
-        <a title="Add Book to Library" className={cardStyle.addBook} href="/">Add Book</a>
+      <a
+        title="Add Book to Library"
+        className={cardStyle.addBook}
+        href="#"
+        onClick={() => onAddClicked(entry)}
+      >
+        Add Book
+      </a>
+    </>
+  );
+}
+
+function SearchBar({ onSearchQueryChange }) {
+  return (
+    <div className={styles.searchContainer}>
+      <div className={styles.searchBar}>
+        <i className={`${styles.icon} fas fa-search ${styles.faSearch}`} />
+        <input
+          type="text"
+          onChange={(e) => onSearchQueryChange(e.target.value)}
+          placeholder="Search Manga/Novel..."
+          name="search"
+          maxLength="50"
+        />
+      </div>
     </div>
   );
 }
-export default function NewBook() {
-    return (
+
+export default function NewBook({ onAddClicked }) {
+
+  const [userInput, setUserInput] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [queryTimeout, setQueryTimeout] = useState(null);
+
+  useEffect(() => {
+    clearTimeout(queryTimeout);
+    if (userInput.length >= 3) {
+      setQueryTimeout(setTimeout(() => {
+        jikanjs
+          .search("manga", userInput)
+          .then(({ results }) => {
+            setSearchResults(
+              results.map((ln) => ({
+                mal_id: ln.mal_id,
+                title: ln.title,
+                coverUrl: ln.image_url,
+                type: ln.type,
+                chapter: 1,
+                volume: 0,
+                status: "Reading",
+              }))
+            );
+          })
+          .catch((e) => console.error(e));
+      }, 600));
+    } else {
+      setSearchResults([]);
+    }
+  }, [userInput]);
+
+  return (
     <div>
-        <div className={styles.main}>
-            <div className={styles.container}>
-                <div className={styles.searchContainer}>
-                    <form action="/" className={styles.searchBar}>
-                        <i className={`${styles.icon} fas fa-search ${styles.faSearch}`} />
-                        <input type="text" placeholder="Search Manga/Novel..." name="search" maxLength="50" />
-                    </form>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <SearchBar onSearchQueryChange={setUserInput} />
+          <CardListWrapper>
+            {searchResults.map((entry) => (
+              <li key={entry.mal_id}>
+                <div className={cardStyle.activityEntry}>
+                  <NovelCard entry={entry} onAddClicked={onAddClicked} />
                 </div>
-                <div className={styles.searchResultContainer}>
-                    <div className={styles.searchResultFeed}>
-                        {data.map(entry => (<NovelCard entry={entry} />))}
-                    </div>
-                </div>
-            </div>
+              </li>
+            ))}
+          </CardListWrapper>
         </div>
+      </div>
     </div>
-    )
+  );
 }
