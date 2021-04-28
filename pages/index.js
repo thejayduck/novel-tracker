e327dfd32a9f259e5ea1a4ff94419c895236f99b
TheJayDuck <therealjayduck@gmail.com>
2021-04-28 23:08:14 +0300

    Adds travis.yml and import&export

--- pages/index.js
+++ pages/index.js
@@ -17,42 +17,42 @@ export function CardListWrapper({ data, children }) {
 function NovelCard({ entry, onIncrement, onDecrement, onInfoClick, onDelete }) {
   return (
     <div className={cardStyle.wrap}>
-        <div className={cardStyle.list}>
-          <p title={entry.title} className={cardStyle.title}>
-            {entry.title}
-          </p>
-          <img className={cardStyle.cover} src={entry.coverUrl} />
-          <div className={cardStyle.details}>
-            <div>
-              <span className={cardStyle.status}>
-                Current Chapter: {entry.chapter}
-              </span>
-              <hr />
-              {/* <span className={styles.status}>Current Volume: {entry.volume}</span>
+      <div className={cardStyle.list}>
+        <p title={entry.title} className={cardStyle.title}>
+          {entry.title}
+        </p>
+        <img className={cardStyle.cover} src={entry.coverUrl} />
+        <div className={cardStyle.details}>
+          <div>
+            <span className={cardStyle.status}>
+              Current Chapter: {entry.chapter}
+            </span>
+            <hr />
+            {/* <span className={styles.status}>Current Volume: {entry.volume}</span>
               <hr/>
               <span className={styles.status}>Status: {entry.status}</span>
               <hr/> */}
-              <a
-                onClick={onIncrement}
-                title="Increase Progress"
-                className="fas fa-plus"
-              />
-              <a
-                onClick={onDecrement}
-                title="Decrease Progress"
-                className="fas fa-minus"
-              />
-              <a onClick={onInfoClick} title="Info" className="fas fa-info"/>
-              <a
-                onClick={onDelete}
-                title="Delete Book"
-                style={{color:'#e33131'}}
-                className="fas fa-trash"
-              />
-            </div>
+            <a
+              onClick={onIncrement}
+              title="Increase Progress"
+              className="fas fa-plus"
+            />
+            <a
+              onClick={onDecrement}
+              title="Decrease Progress"
+              className="fas fa-minus"
+            />
+            <a onClick={onInfoClick} title="Info" className="fas fa-info" />
+            <a
+              onClick={onDelete}
+              title="Delete Book"
+              style={{ color: '#e33131' }}
+              className="fas fa-trash-alt"
+            />
           </div>
         </div>
       </div>
+    </div>
   );
 }
 
@@ -68,14 +68,14 @@ function usePersistedState(key, defaultValue) {
 }
 
 export default function Home() {
-  const [darkmode, setDarkmode] = usePersistedState("darkmode", true);
+  const [darkmode, setDarkmode] = usePersistedState("darkmode", false);
   const [overlay, setOverlay] = useState(false);
   const [data, setData] = useState([]);
   const removeBook = (id) => {
     setData(data.filter(target => target.mal_id !== id));
   };
 
-//#region Fuse
+  //#region Fuse
   const [query, setQuery] = useState('');
 
   const fuse = new Fuse(data, {
@@ -91,24 +91,48 @@ export default function Home() {
   function onSearch({ currentTarget }) {
     setQuery(currentTarget.value);
   }
-//#endregion
+  //#endregion
 
-//#region Local Storage
+  //#region Local Storage
+  const exportData = () => {
+    var element = document.createElement('a');
+    element.style.display = 'none';
+    element.setAttribute('href', 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data)));
+    element.setAttribute('download', 'data.json');
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+
+  const [selectedFile, setSelectedFile] = useState(null); 
+
+  const importData = (target) => {
+    const reader = new FileReader();
+    reader.addEventListener('load', ({target}) => {
+      console.log(target.result);
+      setData(JSON.parse(atob(target.result.replace("data:application/json;base64,", ""))));
+      document.getElementById
+    });
+    reader.readAsDataURL(target);
+  }
+
   useEffect(() => {
     const localData = localStorage.getItem('books');
     setData(localData ? JSON.parse(localData) : []);
   }, [])
 
-  useEffect(()=> {
+  useEffect(() => {
     localStorage.setItem('books', JSON.stringify(data))
   }, [data])
-//#endregion
+  //#endregion
 
   const updateElementInData = (index, updateElementCallback) => {
     let new_data = [...data];
     updateElementCallback(new_data[index]);
     setData(new_data);
   };
+
+
 
   return (
     <div
@@ -122,6 +146,7 @@ export default function Home() {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
         />
         <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="public/favicon.ico" />
@@ -136,8 +161,8 @@ export default function Home() {
               placeholder="Search..."
               name="search"
               maxLength="50"
-              value={query} 
-              onChange={onSearch}         
+              value={query}
+              onChange={onSearch}
             />
           </div>
         </div>
@@ -145,23 +170,23 @@ export default function Home() {
           {bookResults.map((entry, index) => (
             <li key={entry.mal_id}>
               <div className={cardStyle.activityEntry}>
-                  <NovelCard
-                    entry={entry}
-                    onIncrement={() =>
-                      updateElementInData(index, (element) => element.chapter++)
-                    }
-                    onDecrement={() =>
-                      updateElementInData(
-                        index,
-                        (element) =>
-                          (element.chapter = Math.max(0, element.chapter - 1))
-                      )
-                    }
-                    onInfoClick={() => {
-                      console.log("Info");
-                    }}
-                    onDelete={()=> removeBook(entry.mal_id)}
-                  />
+                <NovelCard
+                  entry={entry}
+                  onIncrement={() =>
+                    updateElementInData(index, (element) => element.chapter++)
+                  }
+                  onDecrement={() =>
+                    updateElementInData(
+                      index,
+                      (element) =>
+                        (element.chapter = Math.max(0, element.chapter - 1))
+                    )
+                  }
+                  onInfoClick={() => {
+                    console.log("Info");
+                  }}
+                  onDelete={() => removeBook(entry.mal_id)}
+                />
               </div>
             </li>
           ))}
@@ -170,10 +195,18 @@ export default function Home() {
       <footer
         className={`${styles.footer} ${darkmode ? styles.dark : styles.light}`}
       >
-        <button className={`fas fa-moon ${styles.themeToggle}`} onClick={() => setDarkmode(!darkmode)}/>
+        <button title="Toggle Theme" className={`fas fa-moon ${styles.themeToggle}`} onClick={() => setDarkmode(!darkmode)} />
+        <button title="Export Data" className={`fas fa-download ${styles.themeToggle}`} onClick={exportData} />
+        <button title="Import Data" className={`fas fa-upload ${styles.themeToggle}`} 
+          onClick={() => {
+            var element = document.getElementById('importData');
+            element.click();
+          }} 
+        />
+        <input id="importData" style={{display: 'none'}} type="file" accept=".json" onChange={({target}) => {importData(target.files[0]); target.value=null}}/>
         <p>Books: {data.length}</p>
-        <p>Volumes Read: {data.reduce((acc, val) => acc+val.volume, 0)}</p>
-        <p>Chapters Read: {data.reduce((acc, val) => acc+val.chapter, 0)}</p>
+        <p>Volumes Read: {data.reduce((acc, val) => acc + val.volume, 0)}</p>
+        <p>Chapters Read: {data.reduce((acc, val) => acc + val.chapter, 0)}</p>
         <a
           title="Check Out TheJayDuck's Github"
           href="https://github.com/thejayduck"
@@ -188,10 +221,11 @@ export default function Home() {
         />
       </footer>
       {overlay && (
-        <NewBook onAddClicked={(entry) => { 
-          if(!data.includes(entry))
-            setData([...data, entry])} 
-        }/>
+        <NewBook onAddClicked={(entry) => {
+          if (!data.includes(entry))
+            setData([...data, entry])
+        }
+        } />
       )}
       <div className={styles.newBook} onClick={() => setOverlay(!overlay)}>
         <a title="Add New Book" className="fas fa-plus" />
