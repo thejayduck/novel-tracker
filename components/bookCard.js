f8ac09d0361df40fab6c158cd91557fa8b20f041
thejayduck <therealjayduck@gmail.com>
2021-06-10 23:54:05 +0300

    Improves quick edit buttons for mobile

--- components/bookCard.js
+++ components/bookCard.js
@@ -1,36 +1,38 @@
-import cardStyle from '../styles/BookCard.module.css'
+import styles from '../styles/BookCard.module.css'
 
 export default function BookCard({ entry, onIncrement, onDecrement, onInfoClick, onDelete }) {
     return (
-        <div className={cardStyle.wrap}>
-            <div className={cardStyle.list}>
-                <p title={entry.title} className={cardStyle.title}>
+        <div className={styles.wrap}>
+            <div className={styles.list}>
+                <p title={entry.title} className={styles.title}>
                     {entry.title}
                 </p>
-                <img className={cardStyle.cover} src={entry.coverUrl} />
-                <div className={cardStyle.details}>
+                <img className={styles.cover} src={entry.coverUrl} />
+                <div className={styles.details}>
                     <div>
-                        <span className={cardStyle.status}>
+                        <span className={styles.status}>
                             Current Chapter: {entry.chapter}
                         </span>
                         <hr />
-                        <a
-                            onClick={onIncrement}
-                            title="Increase Progress"
-                            className="fas fa-plus"
-                        />
-                        <a
-                            onClick={onDecrement}
-                            title="Decrease Progress"
-                            className="fas fa-minus"
-                        />
-                        <a onClick={onInfoClick} title="Info" className="fas fa-info" />
-                        <a
-                            onClick={onDelete}
-                            title="Delete Book"
-                            style={{ color: '#e33131' }}
-                            className="fas fa-trash-alt"
-                        />
+                        <div className={styles.quickEdit}>
+                            <a
+                                onClick={onIncrement}
+                                title="Increase Progress"
+                                className="fas fa-plus"
+                            />
+                            <a
+                                onClick={onDecrement}
+                                title="Decrease Progress"
+                                className="fas fa-minus"
+                            />
+                            <a onClick={onInfoClick} title="Info" className="fas fa-info" />
+                            <a
+                                onClick={onDelete}
+                                title="Delete Book"
+                                style={{ color: '#e33131' }}
+                                className="fas fa-trash-alt"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
