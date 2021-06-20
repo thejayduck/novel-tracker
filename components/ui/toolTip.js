04cddb9e28295a112320c8a1f0aeb25dcf97734c
Nobbele <realnobbele@gmail.com>
2021-06-20 22:38:44 +0200

    Fixes tooltip

--- components/ui/toolTip.js
+++ components/ui/toolTip.js
@@ -2,34 +2,18 @@ import styles from '../../styles/components/ToolTip.module.css'
 import React from 'react';
 
 export default function ToolTip({ children, toolTip }) {
-    console.log(React.Children.count(children));
+    const child = React.Children.only(children);
 
-    const childMap = React.Children.map(children, child => {
-        <addInfoIcon>
-            {child}
-        </addInfoIcon>
-    });
-
-    console.log(childMap)
     return (
         <>
             {toolTip == undefined
                 ? children :
                 <div class={styles.toolTip}>
-                    {/* {childMap} */}
-                    <span span className={styles.toolTipText}>{toolTip}</span>
+                    {child}
+                    <i class="far fa-question-circle" />
+                    <span className={styles.toolTipText}>{toolTip}</span>
                 </div>
             }
         </>
     );
-}
-
-export function addInfoIcon({ children }) {
-    return (
-        <>
-            {children}
-            <i class="far fa-question-circle" />
-        </>
-    );
-
 }
