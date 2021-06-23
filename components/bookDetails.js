717d26d07bcb824ce821fd9dcebaa230ed904a3b
TheJayDuck <therealjayduck@gmail.com>
2021-06-23 15:35:19 +0300

    Remakes bookDetail.js

--- components/bookDetails.js
+++ components/bookDetails.js
@@ -31,34 +31,39 @@ export default function BookDetails({ book, onExit, onAddClicked, onOutsideClick
                 className={`${styles.container} ${state.darkMode ? styles.dark : styles.light}`}
                 close={onOutsideClicked}
             >
-                <div className={styles.infoWrapper}>
-                    <div className={styles.banner}>
-                        <img src={book.banner_url} />
-                    </div>
-                    <div className={styles.coverWrapper}>
-                        <img className={styles.cover} src={book.cover_url} />
-                        <Button title="Add Book" onClick={() => addBook()} />
-                        <br />
-                        <Button title="Edit Book" href={`/submitBook?id=${book.book_id}`} />
-                    </div>
-                    <div className={styles.info}>
-                        <h1 title={book.title} className={styles.title}> {book.title} </h1>
-                        <ul>
-                            <li><a>Title (Romanized): {book.title_romanized}</a></li>
-                            <li><a>Title (Native): {book.title_native}<hr /></a></li>
-                            <li><a>Total Volumes: 10</a></li>
-                            <li><a>Total Chapters: 10<hr /></a></li>
-                            <li><a>Author: {book.author}</a></li>
-                        </ul>
+                <div>
+                    <div className={styles.headerWrap}>
+                        <img className={styles.banner} src={book.banner_url} />
+                        <div className={styles.header}>
+                            <div className={styles.coverWrapper}>
+                                <img className={styles.cover} src={book.cover_url} />
+                                <br />
+                                <div className={styles.buttonWrap}>
+                                    <Button title="Add" onClick={() => addBook()} />
+                                    <br />
+                                    <Button title="Edit" href={`/submitBook?id=${book.book_id}`} />
+                                    <br />
+                                </div>
+                            </div>
+                            <div className={styles.titleWrapper}>
+                                <ul className={styles.detailList}>
+                                    <h2 className={styles.title} title={book.title}> {book.title} </h2>
+                                    <li><a>Title (Romanized): {book.title_romanized}</a></li>
+                                    <li><a>Title (Native): {book.title_native}<hr /></a></li>
+                                    <li><a>Total Volumes: 10</a></li>
+                                    <li><a>Total Chapters: 10<hr /></a></li>
+                                    <li><a>Author: {book.author}</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        {book.description &&
+                            <div className={styles.descriptionWrap}>
+                                <h2>Description</h2>
+                                <p className={styles.description}>{book.description}</p>
+                            </div>
+                        }
                     </div>
                 </div>
-                <br />
-                {book.description &&
-                    <div className={styles.descriptionWrapper}>
-                        <h2>Description</h2>
-                        <p className={styles.description}>{book.description}</p>
-                    </div>
-                }
                 <Button title="Exit" onClick={() => onExit()} />
             </OverlayMenu>
         </div>
