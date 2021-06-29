25b5c1bd186717d1c9c742fef1ebd1213af1dfeb
nobbele <realnobbele@gmail.com>
2021-06-29 23:08:44 +0200

    fixes build

--- next.config.js
+++ next.config.js
@@ -3,4 +3,21 @@ module.exports = {
         locales: ['en-US'],
         defaultLocale: 'en-US',
     },
+    webpack: (config, { isServer, webpack }) => {
+        if (!isServer) {
+            config.plugins.push(
+                new webpack.IgnorePlugin({
+                    checkResource(resource, _context) {
+                        // ??
+                        if (resource.includes("pg")) {
+                            return true;
+                        }
+                        return false;
+                    },
+                }),
+            );
+        }
+
+        return config;
+    },
 }
