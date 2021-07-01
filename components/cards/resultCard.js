dc3d647414e51c7236a1a2f363cfe925b443bf74
TheJayDuck <therealjayduck@gmail.com>
2021-07-01 14:59:33 +0300

    Changes on cardelement

--- components/cards/resultCard.js
+++ components/cards/resultCard.js
@@ -4,24 +4,22 @@ import Button from '../ui/button';
 
 export default function ResultCard({ entry, onClick }) {
     return (
-        <>
-            <CardElement entry={entry}>
-                <img className={styles.cover} src={entry?.coverUrl} />
-                <div
-                    className={styles.viewDetailsWrap}
-                    onClick={() => onClick(entry)}
-                >
-                    <a className={styles.viewDetails} title="View Details">
-                        View Details
-                    </a>
-                    <div className={styles.buttonWrap}>
-                        <Button text="Add Book" icon="fas fa-fw fa-plus" onClick={(e) => {
-                            window.alert("Unimplemented");
-                            e.stopPropagation();
-                        }} />
-                    </div>
+        <CardElement entry={entry}>
+            <img className={styles.cover} src={entry?.coverUrl} />
+            <div
+                className={styles.viewDetailsWrap}
+                onClick={() => onClick(entry)}
+            >
+                <a className={styles.viewDetails} title="View Details">
+                    View Details
+                </a>
+                <div className={styles.buttonWrap}>
+                    <Button text="Add Book" icon="fas fa-fw fa-plus" onClick={(e) => {
+                        window.alert("Unimplemented");
+                        e.stopPropagation();
+                    }} />
                 </div>
-            </CardElement>
-        </>
+            </div>
+        </CardElement>
     );
 }
