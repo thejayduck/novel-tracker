87a57c801cdace3f16648d42594d273d98195215
nobbele <realnobbele@gmail.com>
2021-07-02 15:06:21 +0200

    Fixes layout shift on load

--- components/cards/cardListWrapper.js
+++ components/cards/cardListWrapper.js
@@ -1,10 +1,9 @@
 import styles from '../../styles/components/CardElement.module.css'
-import { motion } from 'framer-motion';
 
 export default function CardListWrapper({ children }) {
     return (
-        <motion.ul layout className={styles.cardListFeed}>
+        <ul className={styles.cardListFeed}>
             {children}
-        </motion.ul>
+        </ul>
     );
 }
