0621bf3282ebe5444d6cbe75d686b27f280f0846
TheJayDuck <therealjayduck@gmail.com>
2021-08-26 21:13:41 +0300

    Removes unnecessary elements

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 import PageBase from '../components/pageBase';
 
 
 export default function Home({ user_info }) {
-
   return (
     <PageBase>
+
     </PageBase>
   );
 }
