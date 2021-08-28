c96c946995ce5c4a56ba2db92d5c82af785261d2
TheJayDuck <therealjayduck@gmail.com>
2021-08-28 13:06:00 +0300

    Fixes typo

--- components/navigation.js
+++ components/navigation.js
@@ -29,7 +29,7 @@ export default function Navigation({ data }) {
             </div>
 
             {notification &&
-                <DesktopOverlay title={`Notificaion (1)`} className={styles.notificationOverlay} >
+                <DesktopOverlay title={`Notification (1)`} className={styles.notificationOverlay} >
                     <NotificationItem />
                 </DesktopOverlay>
             }
