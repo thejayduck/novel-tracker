7b6b363bc9d36c7d2af13dafdd017eb545f52f0d
Nobbele <realnobbele@gmail.com>
2021-10-15 15:45:55 +0200

    Redirects to user profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,75 +1,35 @@
 // @ts-nocheck
-import styles from "styles/Home.module.scss";
-
-import dynamic from "next/dynamic";
 import Head from "next/head";
-import { AnimatePresence, motion } from "framer-motion";
 
-import React, { useState } from "react";
+import React from "react";
 
 // Components
 import PageBase from "components/pageBase";
-import { Subtitle } from "components/subtitle";
 
 import { serverSide_checkAuth } from "lib/serverHelpers";
-
-
-const BookCard = dynamic(() => import("../components/cards/card"));
 
 export async function getServerSideProps(context) {
   const [redirect, info] = await serverSide_checkAuth(context, true, false, false);
 
+  console.log(info);
+
   return redirect || {
-    props: {
-      user_info: info,
-    },
+    redirect: {
+      destination: `/user/${info.user_id}`,
+      permanent: false,
+    }
   };
 }
 
-export default function Home({ user_info }) {
+export default function Home({ }) {
   return (
     <>
       <Head>
         <title>Library · Novel Tracker</title>
       </Head>
-      <PageBase user_info={user_info}>
-        <div className="flex flexColumn">
-          <Section title="Reading" icon="bx bx-book-open" />
-          <Section title="Planning" icon="bx bx-bookmarks" />
-          <Section title="Finished" icon="bx bx-check" />
-          <Section title="Dropped" icon="bx bx-trash-alt" />
-        </div>
+      <PageBase user_info={null}>
+        <p>Redirecting to profile...</p>
       </PageBase>
     </>
-  );
-}
-
-function Section({ icon, title }) {
-  const [isOpen, setIsOpen] = useState(false);
-  const book_ids = [];
-
-  return (
-    <motion.div onClick={() => setIsOpen(prev => !prev)} className={`${styles.sectionItem} flex flexColumn`}>
-      <Subtitle text={`${title} [20]`} icon={icon} />
-      <AnimatePresence>
-        {isOpen &&
-          <motion.div
-            className={`${styles.sectionItemList} flex flexRow`}
-            initial={{ height: 0, opacity: 0 }}
-            animate={{ height: "max-content", opacity: 1 }}
-            exit={{ height: 0, opacity: 0 }}
-
-            transition={{ duration: 0.5 }}
-          >
-            <BookCard />
-            {
-              book_ids.map(book_id => (
-                <BookCard key={book_id} book_id={book_id} />
-              ))
-            }
-          </motion.div>
-        }
-      </AnimatePresence>
-    </motion.div>
   );
 }
