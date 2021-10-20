129aa67d456817c61acf9bf9ac7038afb3633b24
thejayduck <ardafevzi.armutcu@gmail.com>
2021-10-20 11:01:10 +0300

    Adds Library Card [WIP]

--- components/pageComponents/statisticItem.tsx
+++ components/pageComponents/statisticItem.tsx
@@ -4,7 +4,7 @@ import { AnimatePresence, motion } from "framer-motion";
 
 import React, { useEffect, useState } from "react";
 
-import Card from "components/cards/card";
+import Card from "components/cards/LibraryCard";
 
 interface StatisticItemProps {
   icon: string,
